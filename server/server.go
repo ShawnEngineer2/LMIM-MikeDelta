ac8f9c903745e235ecc2e5aa0d98a2168e0a658b
ShawnEngineer2 <101727939+ShawnEngineer2@users.noreply.github.com>
2022-07-14 12:01:34 -0400

    Geo Service Work

--- server/server.go
+++ server/server.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 
 	"lemonde.mikedelta/server/services/geoservice"
 )
@@ -20,22 +21,20 @@ func IsAlive() {
 //Initializes the REST API server and endpoint routing
 func InitAPIServer() {
 
-	//Initialize database connection pool
-	print("Initializing database connection pool ....")
-
 	//Put these in Key Vault for next demo
 	dsn := "host=localhost user=lemonde_dev password=LeMonde dbname=LMIMMasterData port=5432 sslmode=disable"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	//Initialize database context for "geo" schema
+	print("Initializing database context for schema geo ....")
+
+	geoDB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			TablePrefix:   "geo.",
+			SingularTable: false,
+		},
+	})
 
 	GenericErrorHandler(err, "Cannot Connect to DB")
-
-	intDB, err := db.DB() //Get an interface to DB for setting the connection pool
-
-	GenericErrorHandler(err, "Cannot Connect to DB")
-
-	intDB.SetMaxIdleConns(10)
-	intDB.SetMaxOpenConns(50)
-	dbConnPool = db
 
 	print("Database Connection Pool Initialized")
 
@@ -45,8 +44,12 @@ func InitAPIServer() {
 	//Create routing group and routes for Geographic information
 	geo_group := api.Group("/Geographic")
 
-	geo_group.Get("/GetCountryList", func(c *fiber.Ctx) error {
-		return c.SendString(geoservice.GetCountry())
+	geo_group.Get("/GetAllCountries", func(c *fiber.Ctx) error {
+		return c.SendString(geoservice.GetAllCountries(geoDB))
+	})
+
+	geo_group.Get("/GetCountry", func(c *fiber.Ctx) error {
+		return c.SendString("Not Implemented")
 	})
 
 	app.Listen(":3000")
--- server/services/geoservice/country.go
+++ server/services/geoservice/country.go
@@ -2,9 +2,11 @@ package geoservice
 
 import (
 	"lemonde.mikedelta/server/models/geo"
+
+	"gorm.io/gorm"
 )
 
-func GetCountry() string {
+func GetAllCountries(*gorm.DB) string {
 	var city = geo.City{}
 	city.Cityid = 1
 	return "Geo-GetCountry"
